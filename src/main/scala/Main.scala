363921f237792564f210b7c2caa5968763ef7ace
Shivansh Rai <shivanshrai84@gmail.com>
2016-05-10 23:45:05 +0530

    Removed map() and instead using filter()

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,16 +11,16 @@ object Main extends App {
   implicit val materializer = ActorMaterializer()
 
   val printingSink = Sink.foreach(println)
-  val primeSieve: Flow[Int, Int, NotUsed] = Flow[Int].map(mappingfxn) 
+  val primeSieve: Flow[Int, Int, NotUsed] = Flow[Int].filter(isPrime(_))
 
-  def mappingfxn(x: Int): Int = {
-    if(isPrime(x)) x
-  }
+  // def mappingfxn(x: Int): Int= {
+  //   if(isPrime(x)) x
+  // }
 
   def isPrime(x: Int): Boolean = {
-    if (x==1) true
+    if (x==1) false
     else if (x==2) true
-    else !(2 to (x-1)).exists(n => x%n==0)
+    else !(2 to (x/2)).exists(n => x%n==0)
   }
 
   Source(1 to 100).via(primeSieve).to(printingSink).run()
