f9b9f8d911577f265a9bfcd4e6d4369d97b3af56
Shivansh Rai <shivanshrai84@gmail.com>
2016-05-11 01:21:47 +0530

    Implemented iterative approach

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,18 +11,20 @@ object Main extends App {
   implicit val materializer = ActorMaterializer()
 
   val printingSink = Sink.foreach(println)
-  val primeSieve: Flow[Int, Int, NotUsed] = Flow[Int].filter(isPrime(_))
+  //val primeSieve: Flow[Int, Int, NotUsed] = Flow[Int].filter(isPrime(_))
 
-  // def mappingfxn(x: Int): Int= {
-  //   if(isPrime(x)) x
-  // }
+  var initialSource = Source(1 to 100)
 
-  def isPrime(x: Int): Boolean = {
-    if (x==1) false
-    else if (x==2) true
-    else !(2 to (x/2)).exists(n => x%n==0)
+  var x = 0
+  for (x <- 2 to 100) {
+    initialSource = initialSource.filter{k => k == x || k % x != 0}
   }
 
-  Source(1 to 100).via(primeSieve).to(printingSink).run()
+  // Recursive approach
+  /*filterFxn(k: Int, s: Source): Source = {*/
+    //if (k == 100) initialSource
+    //else filterFxn(k+1, s.filter(x => x == k || x % k != 0))
+  /*}*/
+  initialSource.to(printingSink).run()
 
 }
